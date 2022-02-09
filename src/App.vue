7a42398ca4a158a11121df736517c92582194611
macsimtv <maximelecouturier@gmail.com>
2022-02-09 23:37:24 +0100

    Push 1

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,9 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <h1>
+    Test github actions !
+  </h1>
 </template>
 
 <style>
